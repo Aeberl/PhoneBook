8c9c95ca40d869c335ddb8cf110894f8610aa005
Abel <duabellan@gmail.com>
2018-01-05 05:04:17 +0000

    What do you think?

--- contactApp/phonebook/models.py
+++ contactApp/phonebook/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class People(models.Model):
+    firstName = models.CharField(max_length = 20)
+    secondName = models.CharField(max_length = 20)
+    
+    def __str__(self):
+        return self.firstName + ' ' + self.secondName
+
+class Address(models.Model):
+    Number = models.IntegerField()
+    email = models.CharField(max_length = 40)
+    people = models.ForeignKey(People)
+    
+    def __str__(self):
+        return self.Number + 'and ' + self.email
